8f1d226ec10aaad517261476d7535c6745dae9a4
Ashley <ashley@victorianfox.com>
2015-10-01 22:46:31 +0200

    Config + IMC

--- src/main/java/foxie/blockquarry/BlockQuarry.java
+++ src/main/java/foxie/blockquarry/BlockQuarry.java
@@ -1,11 +1,9 @@
 package foxie.blockquarry;
 
+import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.event.FMLServerStartedEvent;
+import cpw.mods.fml.common.event.*;
 import foxie.blockquarry.proxy.ProxyCommon;
 
 @Mod(modid = BlockQuarry.MODID, name = BlockQuarry.NAME, version = BlockQuarry.VERSION)
@@ -41,10 +39,39 @@ public class BlockQuarry {
    @Mod.EventHandler
    public void postinit(FMLPostInitializationEvent event) {
       proxy.postinit(event);
+      config.postinit();
    }
 
    @Mod.EventHandler
    public void onServerStarted(FMLServerStartedEvent event) {
+   }
+
+   /**
+    * API option, registering a preset ore. Should happen in the constructor or preinit TOPS, before anything is
+    * registered in the OreDictionary!
+    */
+   @Mod.EventHandler
+   public void onIMCEvent(FMLInterModComms.IMCEvent event) {
+      for (FMLInterModComms.IMCMessage message : event.getMessages()) {
+         if ("preconfiguredOre".equals(message.key)) {
+            if (!message.isStringMessage()) {
+               FMLLog.bigWarning("Mod " + message.getSender() + " tried registering a new ore default without sending the proper data :(");
+               continue;
+            }
+            String[] data = message.getStringValue().split(" ");
+            if (data.length != 6) {
+               FMLLog.bigWarning("Mod " + message.getSender() + " sent an invalid preconfigured ore option!");
+               continue;
+            }
+
+            try {
+               config.registerPreset(data[0], new Config.PreconfiguredOre(Integer.parseInt(data[1]), Integer.parseInt(data[2]),
+                       Integer.parseInt(data[3]), Integer.parseInt(data[4]), Float.parseFloat(data[5])));
+            } catch (Exception e) {
+               FMLLog.bigWarning("Mod " + message.getSender() + " sent an invalid preconfigured ore option, almost made me crash!");
+            }
+         }
+      }
    }
 
 }
--- src/main/java/foxie/blockquarry/Config.java
+++ src/main/java/foxie/blockquarry/Config.java
@@ -9,7 +9,9 @@ import net.minecraftforge.oredict.OreDictionary;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Config {
    public static float quarrySizeMiddle = 0.5f;
@@ -18,7 +20,10 @@ public class Config {
    public static int quarryMinY = 80;
    public static int quarryMaxY = 150;
 
-   public static List<ConfigGenOre> genOres = new ArrayList<ConfigGenOre>();
+   public static List<ConfigGenOre> genOres;
+
+   PreconfiguredOre              defaultPreconfiguredOre;
+   Map<String, PreconfiguredOre> preconfiguredOreMap;
 
    Configuration configBase;
    Configuration configOres;
@@ -26,12 +31,62 @@ public class Config {
    boolean haveOresBeenLoaded = false;
 
    public Config() {
+      genOres = new ArrayList<ConfigGenOre>();
+      preconfiguredOreMap = new HashMap<String, PreconfiguredOre>();
+      defaultPreconfiguredOre = new PreconfiguredOre();
+      // init default values
+      setupPresets();
+
       MinecraftForge.EVENT_BUS.register(this);
-      // let's hope nobody registered BEFORE this
+      // load vanilla entries because Forge is a derp and registers them before any mod is constructed... gg
+      for (String ore : OreDictionary.getOreNames()) {
+         tryRegisteringOre(ore, OreDictionary.getOres(ore).get(0));
+      }
+   }
+
+   private void setupPresets() {
+      registerPreset("oreIron", new PreconfiguredOre(1, 255, 8, 1, 0.5f));
+      registerPreset("oreGold", new PreconfiguredOre(1, 30, 8, 1, 0.2f));
+      registerPreset("oreCopper", new PreconfiguredOre(30, 100, 12, 1, 0.4f));
+      registerPreset("oreTin", new PreconfiguredOre(60, 130, 10, 1, 0.25f));
+      registerPreset("oreDiamond", new PreconfiguredOre(1, 20, 8, 1, 0.05f));
+      registerPreset("oreEmerald", new PreconfiguredOre(1, 120, 1, 1, 0.01f));
+      registerPreset("oreLapis", new PreconfiguredOre(1, 30, 4, 1, 0.1f));
+      registerPreset("oreRedstone", new PreconfiguredOre(1, 30, 6, 1, 0.2f));
+      registerPreset("oreQuartz", new PreconfiguredOre(1, 255, 6, 1, 0));
+      registerPreset("oreCoal", new PreconfiguredOre(1, 255, 24, 1, 0.5f));
+      registerPreset("oreAluminium", new PreconfiguredOre(40, 80, 6, 1, 0.3f));
+      registerPreset("oreLead", new PreconfiguredOre(1, 40, 6, 1, 0.2f));
+      registerPreset("oreSilver", new PreconfiguredOre(1, 40, 6, 1, 0.2f));
+      registerPreset("oreRuby", new PreconfiguredOre(20, 60, 3, 1, 0.1f));
+      registerPreset("oreSapphire", new PreconfiguredOre(20, 60, 3, 1, 0.1f));
+      registerPreset("orePeridot", new PreconfiguredOre(20, 60, 3, 1, 0.1f));
+      registerPreset("oreSulfur", new PreconfiguredOre(4, 12, 10, 1, 0.1f));
+      registerPreset("oreNickel", new PreconfiguredOre(1, 10, 2, 1, 0.07f));
+      registerPreset("oreCertus", new PreconfiguredOre(10, 150, 6, 1, 0.1f));
+      registerPreset("oreCertusCharged", new PreconfiguredOre(10, 150, 6, 1, 0.05f));
+      registerPreset("oreUranium", new PreconfiguredOre(5, 120, 1, 1, 0.1f));
+      registerPreset("oreMonazit", new PreconfiguredOre(5, 120, 6, 1, 0.2f));
+      registerPreset("oreIridium", new PreconfiguredOre(1, 10, 1, 1, 0.02f));
+      registerPreset("orePlatinum", new PreconfiguredOre(1, 10, 1, 1, 0.05f));
+      registerPreset("oreSaltpeter", new PreconfiguredOre(80, 100, 4, 1, 0.2f));
+      registerPreset("oreOsmium", new PreconfiguredOre(20, 120, 12, 1, 0.3f));
+      registerPreset("oreThorium", new PreconfiguredOre(1, 10, 1, 1, 0.1f));
+      // (vis crystal) amber
+
+   }
+
+   public void registerPreset(String oreName, PreconfiguredOre preset) {
+      if (preconfiguredOreMap.containsKey(oreName)) {
+         preconfiguredOreMap.remove(oreName); // replace
+         FMLLog.info("Replacing preconfigured ore " + oreName);
+      }
+
+      preconfiguredOreMap.put(oreName, preset);
    }
 
    public void preinit(String path) {
-      // TODO config loader
+      // TODO base config loader
       configBase = new Configuration(getConfigFile(path, "base"));
       configOres = new Configuration(getConfigFile(path, "ores"));
    }
@@ -40,7 +95,7 @@ public class Config {
       if (!(new File(path).exists()))
          (new File(path)).mkdir();
 
-      return new File(path + File.pathSeparator + name + ".cfg");
+      return new File(path + File.separator + BlockQuarry.MODID + File.separator + name + ".cfg");
    }
 
    public void postinit() { // actually loads the config here after all the oredict registrations have been done
@@ -48,7 +103,13 @@ public class Config {
 
       for (ConfigGenOre ore : genOres) {
          // iterate through and load their configs from configOres.get(oreName.clusterSize) etc
+         ore.initFromConfig(configOres);
       }
+
+      if (configOres.hasChanged())
+         configOres.save();
+      if (configBase.hasChanged())
+         configBase.save();
    }
 
    private void tryRegisteringOre(String name, ItemStack stack) {
@@ -56,13 +117,24 @@ public class Config {
          return;
 
       if (haveOresBeenLoaded) {
-         FMLLog.bigWarning("Some mod has just registered " + name + " waaayy too late in postinit! Sorry, this shall be ignored as config has been already loaded!");
+         FMLLog.bigWarning("Some mod has just registered " + name + " waaayy too late in postinit! Sorry, this shall be " +
+                 "ignored as config has been already loaded! ItemStack name: " + stack.getUnlocalizedName());
+         return;
       }
+
+      if (name.equals("oreAluminum"))
+         name = "oreAluminium"; // USA pls
 
       if (!genOres.contains(name)) {
          ConfigGenOre ore = new ConfigGenOre();
          ore.oreName = name;
-         ore.itemStack = stack;
+         ore.itemStack = stack.copy();
+         ore.itemStack.stackSize = 1; // reset
+
+         if (preconfiguredOreMap.containsKey(name))
+            ore.preconfiguredOre = preconfiguredOreMap.get(name);
+         else
+            ore.preconfiguredOre = defaultPreconfiguredOre;
 
          genOres.add(ore);
       }
@@ -74,12 +146,26 @@ public class Config {
    }
 
    public static class ConfigGenOre {
-      public String    oreName;
-      public ItemStack itemStack;
-      public int       minY;
-      public int       maxY;
-      public int       clusterSize; // simply generates clusterSize of blocks at once
-      public float     chance; // fat chance he he he
+      public  String           oreName;
+      public  ItemStack        itemStack;
+      public  int              minY;
+      public  int              maxY;
+      public  int              clusterSize; // simply generates clusterSize of blocks at once
+      public  int              stackSizeMax;
+      public  float            chance; // fat chance he he he
+      private PreconfiguredOre preconfiguredOre;
+
+      public void initFromConfig(Configuration configuration) {
+         minY = configuration.getInt("minY", "ore." + getCutName(), preconfiguredOre.minY, 1, 255, "Min Y of ore" + oreName);
+         maxY = configuration.getInt("maxY", "ore." + getCutName(), preconfiguredOre.maxY, 1, 255, "Max Y of ore" + oreName);
+         clusterSize = configuration.getInt("clusterSize", "ore." + getCutName(), preconfiguredOre.clusterSize, 1, 255, "Cluster size of " + oreName);
+         stackSizeMax = configuration.getInt("stackSizeMax", "ore." + getCutName(), preconfiguredOre.stackSizeMax, 1, 64, "Max stack size of dropped itemstack per block");
+         chance = configuration.getFloat("chance", "ore." + getCutName(), preconfiguredOre.chance, 0, 1f, "Chance of " + oreName + " to spawn");
+      }
+
+      public String getCutName() {
+         return oreName.substring(3).toLowerCase();
+      }
 
 
       @Override
@@ -91,6 +177,30 @@ public class Config {
             return super.equals(o);
 
          return oreName.equals(((ConfigGenOre) o).oreName);
+      }
+
+      @Override
+      public int hashCode() {
+         return oreName.hashCode();
+      }
+   }
+
+   public static class PreconfiguredOre {
+      public int   minY;
+      public int   maxY;
+      public int   clusterSize;
+      public int   stackSizeMax;
+      public float chance;
+
+      public PreconfiguredOre() {
+      }
+
+      public PreconfiguredOre(int minY, int maxY, int clusterSize, int stackSizeMax, float chance) {
+         this.minY = minY;
+         this.maxY = maxY;
+         this.clusterSize = clusterSize;
+         this.stackSizeMax = stackSizeMax;
+         this.chance = chance;
       }
    }
 }
